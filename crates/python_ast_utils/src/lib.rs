f1546a7610074573c74c42bd8179cb50123b079f
LaBatata101 <labatata101@gmail.com>
2025-01-22 17:44:09 -0300

    Add crate with helper functions for the Python AST

---
+++ crates/python_ast_utils/src/lib.rs
@@ -0,0 +1,167 @@
+pub mod nodes;
+
+use std::borrow::Borrow;
+
+use nodes::{NodeWithParent, Nodes};
+use python_ast::{
+    visitor::{self, Visitor},
+    AnyNodeRef, Expr, Stmt,
+};
+use ruff_python_resolver::module_descriptor::ImportModuleDescriptor;
+use ruff_source_file::LineIndex;
+use ruff_text_size::{Ranged, TextRange};
+
+pub fn node_at_row<'a>(
+    nodes: &'a Nodes,
+    offset: u32,
+    index: &LineIndex,
+) -> Option<&'a NodeWithParent<'a>> {
+    let row = index.line_index(offset.into());
+    nodes
+        .iter()
+        .rev()
+        .find(|node| index.line_index(node.range().start()) == row)
+}
+
+pub fn node_at_offset<'a>(nodes: &'a Nodes, offset: u32) -> Option<&'a NodeWithParent<'a>> {
+    nodes
+        .iter()
+        .rev()
+        .find(|node| node.range().contains_inclusive(offset.into()))
+}
+
+pub fn identifier_from_node<'a>(node: &'a AnyNodeRef<'a>, offset: u32) -> Option<&'a str> {
+    Some(match node {
+        AnyNodeRef::NameExpr(python_ast::NameExpr { id, .. }) => id,
+        AnyNodeRef::Parameter(python_ast::Parameter { name, .. }) => name.as_str(),
+        AnyNodeRef::StmtFunctionDef(python_ast::FunctionDefStmt { name, .. })
+            if name.range.contains(offset.into()) =>
+        {
+            name.as_str()
+        }
+        AnyNodeRef::StmtClassDef(python_ast::ClassDefStmt { name, .. })
+            if name.range.contains(offset.into()) =>
+        {
+            name.as_str()
+        }
+        AnyNodeRef::Alias(python_ast::Alias { name, .. }) if name.range.contains(offset.into()) => {
+            if name.contains('.') {
+                identifier_at_offset(name, offset, name.range)?
+            } else {
+                name.as_str()
+            }
+        }
+        AnyNodeRef::StmtImportFrom(python_ast::ImportFromStmt {
+            module: Some(module),
+            ..
+        }) if module.range.contains(offset.into()) => {
+            identifier_at_offset(module, offset, module.range)?
+        }
+        AnyNodeRef::AttributeExpr(python_ast::AttributeExpr { value, attr, .. }) => {
+            if attr.range().contains(offset.into()) {
+                return Some(attr);
+            }
+
+            find_attribute_name_at_offset(value, offset)
+        }
+        _ => return None,
+    })
+}
+
+fn find_attribute_name_at_offset(expr: &Expr, offset: u32) -> &str {
+    match expr {
+        Expr::Attribute(python_ast::AttributeExpr { value, attr, .. }) => {
+            if attr.range().contains(offset.into()) {
+                attr
+            } else {
+                find_attribute_name_at_offset(value, offset)
+            }
+        }
+        Expr::Call(python_ast::CallExpr { func, .. }) => {
+            find_attribute_name_at_offset(func, offset)
+        }
+        Expr::Name(python_ast::NameExpr { id, .. }) => id,
+        _ => unreachable!(),
+    }
+}
+
+/// Determines which identifier in a dot-separated string an offset falls into.
+fn identifier_at_offset(string: &str, offset: u32, range: TextRange) -> Option<&str> {
+    let relative_offset = (offset - range.start().to_u32()) as usize;
+    let mut current_start = 0;
+
+    for part in string.split('.') {
+        let part_end = current_start + part.len();
+
+        if relative_offset >= current_start && relative_offset < part_end {
+            return Some(part);
+        }
+
+        // Move to the next part, +1 for the dot
+        current_start = part_end + 1;
+    }
+
+    None
+}
+
+pub fn create_import_module_descriptor<'a, T>(import_stmt: T) -> ImportModuleDescriptor
+where
+    T: Borrow<AnyNodeRef<'a>>,
+{
+    match import_stmt.borrow() {
+        AnyNodeRef::Alias(alias) => ImportModuleDescriptor {
+            leading_dots: 0,
+            name_parts: alias
+                .name
+                .split('.')
+                .map(|part| part.trim_end().to_string())
+                .filter(|part| !part.is_empty())
+                .collect(),
+            imported_symbols: vec![],
+        },
+        AnyNodeRef::StmtImportFrom(import) => ImportModuleDescriptor {
+            leading_dots: import.level as usize,
+            name_parts: import
+                .module
+                .as_ref()
+                .map(|module| {
+                    module
+                        .split('.')
+                        .map(|part| part.trim_end().to_string())
+                        .filter(|part| !part.is_empty())
+                        .collect()
+                })
+                .unwrap_or(Vec::new()),
+            imported_symbols: import
+                .names
+                .iter()
+                .map(|alias| alias.name.to_string())
+                .collect(),
+        },
+        _ => panic!("Not an import statement"),
+    }
+}
+
+#[derive(Default)]
+pub struct ReturnStmtCollector<'a> {
+    returns: Vec<&'a python_ast::ReturnStmt>,
+}
+
+impl<'a> ReturnStmtCollector<'a> {
+    pub fn collect(&mut self, body: &'a [Stmt]) -> &[&'a python_ast::ReturnStmt] {
+        self.visit_body(body);
+        &self.returns
+    }
+}
+
+impl<'a, 'b> Visitor<'b> for ReturnStmtCollector<'a>
+where
+    'b: 'a,
+{
+    fn visit_stmt(&mut self, stmt: &'b Stmt) {
+        match stmt {
+            Stmt::Return(return_stmt) => self.returns.push(return_stmt),
+            _ => visitor::walk_stmt(self, stmt),
+        }
+    }
+}
---
+++ crates/python_ast_utils/src/nodes.rs
@@ -0,0 +1,258 @@
+use std::ops::{Deref, Index};
+
+use python_ast::{
+    self as ast,
+    visitor::{self, Visitor},
+    AnyNodeRef, AnyParameterRef, Expr, Pattern, Stmt,
+};
+use ruff_index::{newtype_index, IndexVec};
+use serde::{Deserialize, Serialize};
+
+#[newtype_index]
+#[derive(Serialize, Deserialize)]
+pub struct NodeId;
+
+#[derive(Debug)]
+pub struct NodeWithParent<'a> {
+    pub node: AnyNodeRef<'a>,
+    parent: Option<NodeId>,
+}
+
+impl<'a> NodeWithParent<'a> {
+    pub fn parent_id(&self) -> Option<NodeId> {
+        self.parent
+    }
+
+    pub fn node(&self) -> &AnyNodeRef<'a> {
+        &self.node
+    }
+}
+
+impl<'a> Deref for NodeWithParent<'a> {
+    type Target = AnyNodeRef<'a>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.node
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Nodes<'a>(IndexVec<NodeId, NodeWithParent<'a>>);
+
+impl<'a> Nodes<'a> {
+    pub fn insert(&mut self, node: AnyNodeRef<'a>, parent: Option<NodeId>) -> NodeId {
+        self.0.push(NodeWithParent { node, parent })
+    }
+
+    pub fn parend_id(&self, node_id: NodeId) -> Option<NodeId> {
+        self.0[node_id].parent
+    }
+
+    pub fn get(&self, node_id: NodeId) -> Option<&NodeWithParent<'a>> {
+        self.0.get(node_id)
+    }
+
+    pub fn iter(&self) -> std::slice::Iter<'_, NodeWithParent<'_>> {
+        self.0.iter()
+    }
+}
+
+impl<'a> Index<NodeId> for Nodes<'a> {
+    type Output = AnyNodeRef<'a>;
+
+    fn index(&self, index: NodeId) -> &Self::Output {
+        &self.0[index].node
+    }
+}
+
+#[derive(Default)]
+pub struct NodeStack<'a> {
+    curr_node: Option<NodeId>,
+    nodes: Nodes<'a>,
+}
+
+impl<'a> NodeStack<'a> {
+    pub fn build(mut self, ast: &'a [Stmt]) -> Self {
+        self.visit_body(ast);
+        self
+    }
+
+    pub fn get(&self, id: NodeId) -> Option<&NodeWithParent<'a>> {
+        self.nodes.get(id)
+    }
+
+    pub fn nodes(&self) -> &Nodes {
+        &self.nodes
+    }
+
+    fn push_node(&mut self, node: impl Into<AnyNodeRef<'a>>) -> NodeId {
+        let node_id = self.nodes.insert(node.into(), self.curr_node);
+        self.curr_node = Some(node_id);
+        node_id
+    }
+
+    fn pop_node(&mut self) {
+        let node_id = self.curr_node.expect("Attempt to pop without node");
+        self.curr_node = self.nodes.parend_id(node_id);
+    }
+}
+
+impl<'a, 'b> Visitor<'b> for NodeStack<'a>
+where
+    'b: 'a,
+{
+    fn visit_stmt(&mut self, stmt: &'b ast::Stmt) {
+        self.push_node(stmt);
+
+        match stmt {
+            Stmt::FunctionDef(ast::FunctionDefStmt {
+                body,
+                parameters,
+                decorator_list,
+                ..
+            }) => {
+                for decorator in decorator_list {
+                    self.visit_decorator(decorator);
+                }
+                self.visit_parameters(parameters);
+                self.visit_body(body);
+            }
+            Stmt::ClassDef(ast::ClassDefStmt {
+                body,
+                decorator_list,
+                ..
+            }) => {
+                for decorator in decorator_list {
+                    self.visit_decorator(decorator);
+                }
+                self.visit_body(body);
+            }
+            Stmt::ImportFrom(ast::ImportFromStmt { names, .. }) => {
+                for name in names {
+                    self.visit_alias(name);
+                }
+            }
+            _ => visitor::walk_stmt(self, stmt),
+        }
+
+        self.pop_node();
+    }
+
+    fn visit_type_param(&mut self, type_param: &'b ast::TypeParam) {
+        self.push_node(type_param);
+        match type_param {
+            ast::TypeParam::TypeVar(type_var) => {
+                if let Some(bound) = type_var.bound.as_ref() {
+                    self.visit_expr(bound);
+                }
+                if let Some(default) = type_var.default.as_ref() {
+                    self.visit_expr(default);
+                }
+            }
+            ast::TypeParam::ParamSpec(param_spec) => {
+                if let Some(default) = param_spec.default.as_ref() {
+                    self.visit_expr(default);
+                }
+            }
+
+            ast::TypeParam::TypeVarTuple(type_var_tuple) => {
+                if let Some(default) = type_var_tuple.default.as_ref() {
+                    self.visit_expr(default);
+                }
+            }
+        }
+        self.pop_node();
+    }
+
+    fn visit_alias(&mut self, alias: &'b ast::Alias) {
+        self.push_node(alias);
+        self.pop_node();
+    }
+
+    fn visit_except_handler(&mut self, except_handler: &'b ast::ExceptHandler) {
+        self.push_node(except_handler);
+        match except_handler {
+            ast::ExceptHandler::ExceptHandler(ast::ExceptHandlerExceptHandler { body, .. }) => {
+                self.visit_body(body);
+            }
+        }
+        self.pop_node();
+    }
+
+    fn visit_with_item(&mut self, with_item: &'b ast::WithItem) {
+        self.push_node(with_item);
+        if let Some(optional_vars) = with_item.optional_vars.as_ref() {
+            self.visit_expr(optional_vars);
+        }
+        self.visit_expr(&with_item.context_expr);
+        self.pop_node();
+    }
+
+    fn visit_parameters(&mut self, parameters: &'b ast::Parameters) {
+        self.push_node(parameters);
+
+        for parameter_with_default in parameters.iter_non_variadic_params() {
+            self.push_node(parameter_with_default);
+            self.visit_parameter(&parameter_with_default.parameter);
+            if let Some(default) = &parameter_with_default.default {
+                self.visit_expr(default);
+            }
+            self.pop_node();
+        }
+
+        for parameter in parameters.iter().filter_map(AnyParameterRef::as_variadic) {
+            self.visit_parameter(parameter);
+        }
+
+        self.pop_node();
+    }
+
+    fn visit_parameter(&mut self, parameter: &'b ast::Parameter) {
+        self.push_node(parameter);
+        if let Some(annotation) = &parameter.annotation {
+            self.visit_expr(annotation);
+        }
+        self.pop_node();
+    }
+
+    fn visit_pattern(&mut self, pattern: &'b ast::Pattern) {
+        self.push_node(pattern);
+        if let Pattern::MatchSequence(ast::PatternMatchSequence { patterns, .. }) = pattern {
+            for pattern in patterns {
+                self.visit_pattern(pattern);
+            }
+        }
+        self.pop_node();
+    }
+
+    fn visit_comprehension(&mut self, comprehension: &'b ast::Comprehension) {
+        self.push_node(comprehension);
+
+        self.visit_expr(&comprehension.iter);
+        self.visit_expr(&comprehension.target);
+        for expr in &comprehension.ifs {
+            self.visit_expr(expr);
+        }
+
+        self.pop_node();
+    }
+
+    fn visit_expr(&mut self, expr: &'b ast::Expr) {
+        self.push_node(expr);
+        match expr {
+            Expr::Attribute(ast::AttributeExpr { value, .. }) => {
+                self.visit_expr(value);
+            }
+            Expr::Lambda(ast::LambdaExpr {
+                body, parameters, ..
+            }) => {
+                if let Some(parameters) = parameters {
+                    self.visit_parameters(parameters);
+                }
+                self.visit_expr(body);
+            }
+            _ => visitor::walk_expr(self, expr),
+        }
+        self.pop_node();
+    }
+}
